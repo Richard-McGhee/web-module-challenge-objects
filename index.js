27f24820c3bdd5fbff6f3ae409176c4209846356
richard-mcghee <richardjmcghee@yahoo.com>
2020-06-12 00:24:13 -0400

    completed stretch 2

--- index.js
+++ index.js
@@ -145,10 +145,17 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
-    /* code here */
+  function getLongReviews(objArr) {
+    const longReviews = [];
+    for(let i = 0; i < objArr.length; i++){
+      if(objArr[i].feedback.length > 15){
+        longReviews.push(objArr[i]);
+      }
+    }
+    return longReviews
   }
   
+console.log(getLongReviews(reviews));
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
